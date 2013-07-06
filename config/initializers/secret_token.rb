7c53ad22cfdfec905319456a25fe910562462a1b
Luke Robertson <luke.robertson@mac.com>
2013-07-06 09:06:30 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookbookApp::Application.config.secret_token = '3891d5ae4d64df7a724945d2d12c57cfef14af342e2d82ff29c11d2886602ccaed88af99b350fb93479ca7c2408e08abc995b95f777836c322cfb4ef440fe7ac'
